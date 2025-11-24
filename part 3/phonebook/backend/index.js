091b739084a06f8162b4d2427ba66b672d1e51f5
Max Shapovalov <maximsdev@gmail.com>
2025-11-24 14:56:37 -0800

    3.22 ESLint Applied to index.js
    Cleaned up backend with ESLint

--- part 3/phonebook/backend/index.js
+++ part 3/phonebook/backend/index.js
@@ -9,23 +9,23 @@ const Person = require('./models/person')
 app.use(express.static('dist'))
 app.use(express.json())
 
-morgan.token('post-body', (req)=> { return req.method==='POST'? JSON.stringify(req.body) : '' })
+morgan.token('post-body', (req) => { return req.method==='POST'? JSON.stringify(req.body) : '' })
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post-body'))
 
 app.get('/info', (request, response) => {
   Person.countDocuments({}).
-    then(count=>{
-    response.send(`<div>
+    then(count => {
+      response.send(`<div>
     <p>Phone book has info for ${count} people.</p>
     <p>${new Date}</p>
   </div>`)
-  })
-  
+    })
+
 })
 
 app.get('/api/persons', (request, response) => {
-  Person.find({}).then(p=>{
+  Person.find({}).then(p => {
     response.json(p)
   })
 })
@@ -34,19 +34,19 @@ app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id)
     .then(person => {
       if (person) {
-        response.json(person);
+        response.json(person)
       } else {
-        response.status(404).end();
+        response.status(404).end()
       }
     })
-     .catch(error => next(error))
-});
+    .catch(error => next(error))
+})
 
 app.post('/api/persons', (request, response, next) => {
-  const { name, number } = request.body;
+  const { name, number } = request.body
 
   if (!name || !number) {
-    return response.status(400).json({ error: 'name or number missing' });
+    return response.status(400).json({ error: 'name or number missing' })
   }
 
   Person.findOne({ name: new RegExp(`^${name}$`, 'i') })
@@ -54,24 +54,24 @@ app.post('/api/persons', (request, response, next) => {
       if (existingPerson) {
         return Person.findById(existingPerson._id)
           .then(person => {
-            if (!person) throw new Error('Person not found');
-            person.number = number;
-            return person.save();
-          });
+            if (!person) throw new Error('Person not found')
+            person.number = number
+            return person.save()
+          })
       }
 
-      const person = new Person({ name, number });
-      return person.save();
+      const person = new Person({ name, number })
+      return person.save()
     })
     .then(savedPerson => response.json(savedPerson))
     .catch(error => {
       next(error)
-    });
-});
+    })
+})
 
 
-app.put('/api/persons/:id', (request, response, next)=>{
-  const { name, number } = request.body;
+app.put('/api/persons/:id', (request, response, next) => {
+  const { name, number } = request.body
 
   Person.findById(request.params.id)
     .then(person => {
@@ -79,18 +79,18 @@ app.put('/api/persons/:id', (request, response, next)=>{
         person.name=name
         person.number=number
         person.save()
-        .then(updatedPerson=>response.json(updatedPerson))
-        .catch(error=>next(error))
+          .then(updatedPerson => response.json(updatedPerson))
+          .catch(error => next(error))
       } else {
-        response.status(404).end();
+        response.status(404).end()
       }
     })
-     .catch(error => next(error))
+    .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndDelete(request.params.id)
-    .then(result => {
+    .then(response => {
       response.status(204).end()
     })
     .catch(error => next(error))
@@ -103,16 +103,16 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const errorHandler = (error, request, response, next)=>{
+const errorHandler = (error, request, response, next) => {
   console.error(error.message)
 
   if(error.name === 'CastError'){
-    return response.status(400).send({error:'malformatted id'})
+    return response.status(400).send({ error:'malformatted id' })
   }
   else if (error.name === 'ValidationError') {
     return response.status(400).json({ error: error.message })
   }
-  
+
   next(error)
 
 }
