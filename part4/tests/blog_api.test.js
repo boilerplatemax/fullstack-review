e8b56769a04a0cdf07643b95e118583ed2f5c75e
Max Shapovalov <maximsdev@gmail.com>
2025-11-26 10:25:40 -0800

    parts 1-3 completed
    Cleaning test code with prettier

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -1,29 +1,29 @@
-const { test, after } = require('node:test')
-const mongoose = require('mongoose')
-const supertest = require('supertest')
-const app = require('../app')
+const { test, after } = require("node:test")
+const mongoose = require("mongoose")
+const supertest = require("supertest")
+const app = require("../app")
 
 const api = supertest(app)
-const assert = require('node:assert')
+const assert = require("node:assert")
 
-test('blogs are returned as json', async () => {
+test("blogs are returned as json", async () => {
   await api
-    .get('/api/blogs')
+    .get("/api/blogs")
     .expect(200)
-    .expect('Content-Type', /application\/json/)
+    .expect("Content-Type", /application\/json/)
 })
 
-test('all blogs are returned', async () => {
-  const response = await api.get('/api/blogs')
+test("all blogs are returned", async () => {
+  const response = await api.get("/api/blogs")
 
   assert.strictEqual(response.body.length, 2)
 })
 
-test('a specific blog is within the returned blogs', async () => {
-  const response = await api.get('/api/blogs')
+test("a specific blog is within the returned blogs", async () => {
+  const response = await api.get("/api/blogs")
 
-  const titles = response.body.map(e => e.title)
-  assert(titles.includes('How to golf'))
+  const titles = response.body.map((e) => e.title)
+  assert(titles.includes("How to golf"))
 })
 
 after(async () => {
